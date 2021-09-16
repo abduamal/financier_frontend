ba4a526b33a47db236e7e887b8926deb203350e1
Amal Abdu <abduamal@ymail.com>
2021-09-16 13:22:27 -0500

    configure Login

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,29 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Dashboard from '../Dashboard/Dashboard'
 import Login from '../Login/Login'
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import 'style.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
-  const [token, setToken] = useState()
-
-  if(!token) {
-    return <Login setToken={setToken} />
-  }
+  // const [token, setToken] = useState()
+  //
+  // if(!token) {
+  //   return <Login setToken={setToken} />
+  // }
 
   return (
     <div className="App">
       <h1>NAVBAR GOES HERE.</h1>
-      <BrowserRouter>
+      <Router>
         <Switch>
           <Route path="/dashboard">
             <Dashboard/>
           </Route>
+          <Route exact path="/login">
+            <Login/>
+          </Route>
         </Switch>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 }
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,140 +1,150 @@
-import React, { useState } from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Paper from '@material-ui/core/Paper';
-import Box from '@material-ui/core/Box';
-import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import PropTypes from 'prop-types'
+import React from "react";
+import { connect } from 'react-redux';
+import { loginUser } from '../../actions/auth';
+import AnimationRevealPage from 'components/Treact/helpers/AnimationRevealPage';
+import { Container as ContainerBase } from 'components/Treact/components/misc/Layouts';
+import tw from "twin.macro";
+import styled from "styled-components";
+import {css} from "styled-components/macro"; //eslint-disable-line
+import illustration from 'components/Treact/images/login-illustration.svg';
+// import logo from 'components/Treact/images/logo.svg';
+// import googleIconImageSrc from "images/google-icon.png";
+// import twitterIconImageSrc from "images/twitter-icon.png";
+import { ReactComponent as LoginIcon } from "feather-icons/dist/icons/log-in.svg";
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Financier
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
+const Container = tw(ContainerBase)`min-h-screen bg-primary-900 text-white font-medium flex justify-center -m-8`;
+const Content = tw.div`max-w-screen-xl m-0 sm:mx-20 sm:my-16 bg-white text-gray-900 shadow sm:rounded-lg flex justify-center flex-1`;
+const MainContainer = tw.div`lg:w-1/2 xl:w-5/12 p-6 sm:p-12`;
+// const LogoLink = tw.a``;
+// const LogoImage = tw.img`h-12 mx-auto`;
+const MainContent = tw.div`mt-12 flex flex-col items-center`;
+const Heading = tw.h1`text-2xl xl:text-3xl font-extrabold`;
+const FormContainer = tw.div`w-full flex-1 mt-8`;
+
+// const SocialButtonsContainer = tw.div`flex flex-col items-center`;
+// const SocialButton = styled.a`
+//   ${tw`w-full max-w-xs font-semibold rounded-lg py-3 border text-gray-900 bg-gray-100 hocus:bg-gray-200 hocus:border-gray-400 flex items-center justify-center transition-all duration-300 focus:outline-none focus:shadow-outline text-sm mt-5 first:mt-0`}
+//   .iconContainer {
+//     ${tw`bg-white p-2 rounded-full`}
+//   }
+//   .icon {
+//     ${tw`w-4`}
+//   }
+//   .text {
+//     ${tw`ml-4`}
+//   }
+// `;
+
+// const DividerTextContainer = tw.div`my-12 border-b text-center relative`;
+// const DividerText = tw.div`leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform -translate-y-1/2 absolute inset-x-0 top-1/2 bg-transparent`;
+
+const Form = tw.form`mx-auto max-w-xs`;
+const Input = tw.input`w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5 first:mt-0`;
+const SubmitButton = styled.button`
+  ${tw`mt-5 tracking-wide font-semibold bg-primary-500 text-gray-100 w-full py-4 rounded-lg hover:bg-primary-900 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none`}
+  .icon {
+    ${tw`w-6 h-6 -ml-2`}
+  }
+  .text {
+    ${tw`ml-3`}
+  }
+`;
+const IllustrationContainer = tw.div`sm:rounded-r-lg flex-1 bg-purple-100 text-center hidden lg:flex justify-center`;
+const IllustrationImage = styled.div`
+  ${props => `background-image: url("${props.imageSrc}");`}
+  ${tw`m-12 xl:m-16 w-full max-w-sm bg-contain bg-center bg-no-repeat`}
+`;
+
+class Login extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+        email: '',
+        password: '',
+        error: false
+      }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    const { email, password } = this.state
+    this.props
+      .dispatchLoginUser({ email, password })
+      .then(() => this.props.history.push("/"))
+      .catch(() => this.setState({ error: true}))
+  }
+
+  render(){
+    const headingText = 'Log In To Financier'
+    const submitButtonText = 'Log In'
+    const SubmitButtonIcon = LoginIcon
+    const signupUrl = '#'
+    // const logoLinkUrl = '#'
+    return(
+      <AnimationRevealPage>
+        <Container>
+          <Content>
+            <MainContainer>
+              {/* <LogoLink href={logoLinkUrl}>
+                <LogoImage src={logo} />
+              </LogoLink> */}
+              <MainContent>
+                <Heading>{headingText}</Heading>
+                <FormContainer>
+                  {/* <SocialButtonsContainer>
+                    {socialButtons.map((socialButton, index) => (
+                      <SocialButton key={index} href={socialButton.url}>
+                    <span className="iconContainer">
+                    <img src={socialButton.iconImageSrc} className="icon" alt=""/>
+                    </span>
+                    <span className="text">{socialButton.text}</span>
+                      </SocialButton>
+                    ))}
+                    </SocialButtonsContainer>
+                    <DividerTextContainer>
+                    <DividerText>Or Sign in with your e-mail</DividerText>
+                  </DividerTextContainer> */}
+                  <Form onSubmit={this.handleSubmit}>
+                    <Input name="email" type="email" placeholder="Email" onChange={this.handleChange} value={this.state.email}/>
+                    <Input name="password" type="password" placeholder="Password" onChange={this.handleChange} value={this.state.password} />
+                    <SubmitButton type="submit">
+                      <SubmitButtonIcon className="icon" />
+                      <span className="text">{submitButtonText}</span>
+                    </SubmitButton>
+                  </Form>
+                  <p tw="mt-6 text-xs text-gray-600 text-center">
+                    {this.state.error}
+                  </p>
+                  <p tw="mt-8 text-sm text-gray-600 text-center">
+                    Dont have an account?{" "}
+                    <a href={signupUrl} tw="border-b border-gray-500 border-dotted">
+                      Sign Up
+                    </a>
+                  </p>
+                </FormContainer>
+              </MainContent>
+            </MainContainer>
+            <IllustrationContainer>
+              <IllustrationImage imageSrc={illustration} />
+            </IllustrationContainer>
+          </Content>
+        </Container>
+      </AnimationRevealPage>
+    )
+  }
 }
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    height: '100vh',
-  },
-  image: {
-    backgroundImage: 'url(https://images.unsplash.com/photo-1631063640328-96d4e62780ca?ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MXx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60)',
-    backgroundRepeat: 'no-repeat',
-    backgroundColor:
-      theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-  },
-  paper: {
-    margin: theme.spacing(8, 4),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
-
-export default function Login(setToken) {
-  const classes = useStyles()
-  const [username, setUserName] = useState()
-  const [password, setPassword] = useState()
-
-  return (
-    <Grid container component="main" className={classes.root}>
-      <CssBaseline />
-      <Grid item xs={false} sm={4} md={7} className={classes.image} />
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-        <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <form className={classes.form} noValidate>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              onChange={e => setUserName(e.target.value)}
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              onChange={e => setPassword(e.target.value)}
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-            <Box mt={5}>
-              <Copyright />
-            </Box>
-          </form>
-        </div>
-      </Grid>
-    </Grid>
-  );
+const mapDispatchToProps = (dispatch) => {
+  return {
+    dispatchLoginUser: (credentials) => dispatch(loginUser(credentials))
+  }
 }
-
-Login.propTypes = {
-  setToken: PropTypes.func.isRequired
-}
+export default connect(null, mapDispatchToProps)(Login)
